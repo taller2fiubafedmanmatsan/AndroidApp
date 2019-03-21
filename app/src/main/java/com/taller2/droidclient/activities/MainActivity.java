06f655992f145158ba6d0e4abe1ff5c9e0b648fb
Funes Federico <fede.funes96@gmail.com>
2019-03-21 05:47:53 -0300

    Agrego conexion Firebase -> Facebook

--- app/src/main/java/com/taller2/droidclient/activities/MainActivity.java
+++ app/src/main/java/com/taller2/droidclient/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.taller2.droidclient.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,11 +16,19 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.taller2.droidclient.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private CallbackManager callbackManager;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +54,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        auth = FirebaseAuth.getInstance();
+
         //Facebook integration
         callbackManager = CallbackManager.Factory.create();
 
         LoginButton loginButton = findViewById(R.id.login_facebook);
 
-        loginButton.setReadPermissions("email");
+        loginButton.setReadPermissions("email", "public_profile");
         // If using in a fragment
         //loginButton.setFragment(this);
 
@@ -59,11 +70,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
-                Toast.makeText(MainActivity.this, "Logged successfully", Toast.LENGTH_SHORT).show();
-
                 AccessToken token = loginResult.getAccessToken();
 
-               // boolean isLoggedIn = token != null && !token.isExpired();
+                AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+
+                auth.signInWithCredential(credential)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = auth.getCurrentUser();
+
+                                    Toast.makeText(MainActivity.this, "Logged successfully", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Logging failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                // boolean isLoggedIn = token != null && !token.isExpired();
 
 
             }
