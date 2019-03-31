5395d9bef39184a5e4656ed3382284eec7a708ab
Funes Federico <fede.funes96@gmail.com>
2019-03-31 05:49:23 -0300

    Agrego seleccion de imagen de galeria y crop

--- app/src/main/java/com/taller2/droidclient/activities/ProfileActivity.java
+++ app/src/main/java/com/taller2/droidclient/activities/ProfileActivity.java
@@ -1,8 +1,14 @@
 package com.taller2.droidclient.activities;
 
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +19,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.makeramen.roundedimageview.RoundedImageView;
 import com.makeramen.roundedimageview.RoundedTransformationBuilder;
 
@@ -21,17 +32,22 @@ import com.taller2.droidclient.model.CallbackUserRequester;
 import com.taller2.droidclient.model.User;
 import com.taller2.droidclient.requesters.UserRequester;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Response;
 
 import com.google.gson.Gson;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import org.json.JSONObject;
 
 
 public class ProfileActivity extends BasicActivity{
+
+    private final int SELECT_IMAGE = 1;
 
     private EditText user_profile;
     private TextView email_profile;
@@ -39,9 +55,12 @@ public class ProfileActivity extends BasicActivity{
     private Button button_update_profile;
     private ImageView profile_picture;
     private Button button_change_password;
+    private Button button_change_picture;
     private User userdata;
     private String token;
     private UserRequester userRequester;
+
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,12 +70,15 @@ public class ProfileActivity extends BasicActivity{
 
         setContentView(R.layout.activity_profile);
 
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
         user_profile = findViewById(R.id.user_label);
         email_profile = findViewById(R.id.email_label);
         name_profile = findViewById(R.id.name_label);
         button_update_profile = findViewById(R.id.icon_edit_name);
         profile_picture = findViewById(R.id.profile_picture);
         button_change_password = findViewById(R.id.change_password);
+        button_change_picture = findViewById(R.id.icon_edit_image);
 
         token = this.getUserToken();
 
@@ -131,8 +153,21 @@ public class ProfileActivity extends BasicActivity{
                 changeActivity(ProfileActivity.this,ChangePasswordActivity.class,token);
             }
         });
+
+        button_change_picture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open_gallery();
+            }
+        });
     }
 
+    private void open_gallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"),SELECT_IMAGE);
+    }
 
     private void update_profile(String newNick) {
         userRequester.changeNicknameUser(newNick, token, new CallbackUserRequester() {
@@ -167,6 +202,35 @@ public class ProfileActivity extends BasicActivity{
 
     }
 
+    private void changeProfilePicture(Bitmap bitmap) {
+        String image_name = "profile-" + userdata.getId() + ".jpg";
+        StorageReference mountainsRef = mStorageRef.child(image_name);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        UploadTask uploadTask = mountainsRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Toast.makeText(ProfileActivity.this, "Try again later", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
+                //Send to server pending
+
+                Glide.with(ProfileActivity.this)
+                        .load(downloadUrl).into(profile_picture);
+            }
+        });
+
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -176,5 +240,38 @@ public class ProfileActivity extends BasicActivity{
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_IMAGE) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (data != null) {
+                    //try {
+                        //Bitmap bitmap = MediaStore.Images.Media.getBitmap(ProfileActivity.this.getContentResolver(), data.getData());
+                    CropImage.activity(data.getData())
+                            .start(ProfileActivity.this);
+                        // changeProfilePicture(bitmap);
+                    /*} catch (IOException e) {
+                        e.printStackTrace();
+                    }*/
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED)  {
+                Toast.makeText(ProfileActivity.this, "Cancelled", Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(ProfileActivity.this.getContentResolver(), resultUri);
+                    changeProfilePicture(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 }
