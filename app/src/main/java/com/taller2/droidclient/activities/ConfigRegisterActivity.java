1902ee4d8cc3aad4f3b24061a8d10dadaa620509
Funes Federico <fede.funes96@gmail.com>
2019-04-04 05:31:10 -0300

    Agrego nueva pantalla de modificacion de perfil y ahora los cambios en las imagenes de perfil son permanentes

---
+++ app/src/main/java/com/taller2/droidclient/activities/ConfigRegisterActivity.java
@@ -0,0 +1,13 @@
+package com.taller2.droidclient.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfigRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config_register);
+    }
+}
