3f583ef94964a955712f1af192bcb09bb18eebfd
MatiasRei <matiasreimondo@gmail.com>
2019-04-30 20:33:46 -0300

    solucion canales

--- app/src/main/java/com/taller2/droidclient/activities/ChatActivity.java
+++ app/src/main/java/com/taller2/droidclient/activities/ChatActivity.java
@@ -270,7 +270,7 @@ public class ChatActivity extends BasicActivity
                             @Override
                             public void run() {
                                 Channel actual_channel = preference.getActualChannel();
-                                if (!actualChannels.contains(actual_channel.getName())) {
+                                if (!actualChannels.contains(actual_channel) && !actual_channel.getName().isEmpty()) {
                                     actualChannels.add(actual_channel);
                                 }
                                 ChannelListAdapter adapter = new ChannelListAdapter(ChatActivity.this, actualChannels);
@@ -423,6 +423,7 @@ public class ChatActivity extends BasicActivity
                     WorkspaceResponse selectedItem = (WorkspaceResponse) parent.getItemAtPosition(position);
                     String workName = selectedItem.getName();
                     preference.saveActualWorkspace(new WorkspaceResponse(workName));
+                    preference.saveActualChannel(new Channel(""));
                     changeActivity(ChatActivity.this, StartLoadingActivity.class);
                 }catch (Exception e){
                     Log.d("ERROR", e.getMessage());
