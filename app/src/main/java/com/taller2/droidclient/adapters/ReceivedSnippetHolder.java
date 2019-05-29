05b2479dd66faa4304540b6b40d4f2d49e2f5e3f
Funes Federico <fede.funes96@gmail.com>
2019-05-29 20:57:26 -0300

    Added final version of snippet

--- app/src/main/java/com/taller2/droidclient/adapters/ReceivedSnippetHolder.java
+++ app/src/main/java/com/taller2/droidclient/adapters/ReceivedSnippetHolder.java
@@ -1,6 +1,9 @@
 package com.taller2.droidclient.adapters;
 
 import android.graphics.Color;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
@@ -19,7 +22,7 @@ import static com.taller2.droidclient.utils.GlideOptions.centerCropTransform;
 
 public class ReceivedSnippetHolder extends MessageListAdapter.MyViewHolder {
     private TextView messageText, timeText, nameText;
-    private TextView numbers;
+    private final TextView numbers;
     private EditText code;
     private View context;
     private CodeView codeView;
@@ -30,12 +33,12 @@ public class ReceivedSnippetHolder extends MessageListAdapter.MyViewHolder {
         context = itemView;
 
         nameText = (TextView) itemView.findViewById(R.id.text_message_name);
-        codeView = (CodeView) itemView.findViewById(R.id.code_view);
-        /*numbers = (TextView) itemView.findViewById(R.id.code_number);
+        //codeView = (CodeView) itemView.findViewById(R.id.code_view);
+        numbers = (TextView) itemView.findViewById(R.id.code_number);
         code = (EditText) itemView.findViewById(R.id.code_view);
 
         code.setFocusable(false);
-        code.setClickable(true);*/
+        code.setClickable(true);
 
         /*ScrollView scrollView = (ScrollView) itemView.findViewById(R.id.scrollView_code);
 
@@ -49,21 +52,64 @@ public class ReceivedSnippetHolder extends MessageListAdapter.MyViewHolder {
                 return true;
             }
         });*/
+
     }
 
     void bind(UserMessage message) {
         nameText.setText(message.getCreator().getNickname());
 
-        codeView.setCode(message.getMessage(), "py");
-        /*code.setText(message.getMessage());
+        /*code.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-        StringBuilder linesText = new StringBuilder("");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int lines = code.getLineCount();
+                StringBuilder linesText = new StringBuilder("");
+
+                for (int i = 1; i <= lines; i++) {
+                    //linesText.concat("\n" + i);
+                    linesText.append(i).append("\n");
+                }
+
+                numbers.setText(linesText.toString());
+
+                Log.d("NUMBERLINES1", String.valueOf(code.getLineCount()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.d("NUMBERLINES2", String.valueOf(code.getLineCount()));
+            }
+        });*/
+        //codeView.setCode(message.getMessage(), "py");
+        code.setText(message.getMessage());
+        code.post(new Runnable() {
+            @Override
+            public void run() {
+                int lineCnt = code.getLineCount();
+
+                StringBuilder linesText = new StringBuilder("");
+
+                for (int i = 1; i <= lineCnt; i++) {
+                    linesText.append(i).append("\n");
+                    Log.d("Appending", "True");
+                }
+
+                numbers.setText(linesText.toString());
+            }
+        });
+
+        /*StringBuilder linesText = new StringBuilder("");
 
         for (int i = 1; i <= code.getLineCount(); i++) {
             linesText.append(i).append("\n");
-        }
+            Log.d("Appending", "True");
+        }*/
 
-        numbers.setText(linesText.toString());*/
+        //numbers.setText("1");
 
 
     }
